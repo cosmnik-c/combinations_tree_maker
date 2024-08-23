d02725b8871252a3df43d032f571268a256069dc
cosmnik-c <cosmnik@gmail.com>
2024-08-23 13:23:55 +0700

    minor patch
    use graphviz.escape() on element names to make any element with a backward slash, <, or > in it's name not break stuff

--- gen.py
+++ gen.py
@@ -6,7 +6,7 @@ from graphviz import Digraph
 
 with open("./combinations.json", "r") as file:
     rec1 = json.load(file)
-    recipes = {e["id"]: e for e in rec1} 
+    recipes = {e["id"]: e for e in rec1} #makes it so that you don't have to iterate over every element to find an id
 
 def getElementTree(id):
     if len(recipes[id]["parents"]) != 2: return id
@@ -14,24 +14,27 @@ def getElementTree(id):
 
 
 dot = Digraph()
-def visualizeTree(tree: dict, saved:set = set()): #i have no idea what i'm doing
+def visualizeTree(tree: dict, saved:set = set()):
     id = tree if type(tree) in (float, int) else list(tree.keys())[0]
     if id in saved: return
     saved.add(id)
     
-    dot.node(str(id), recipes[id]["name"])
-    if type(tree) in (float, int): return
 
+    dot.node(str(id), graphviz.escape(recipes[id]["name"])) #use graphviz.escape to prevent elements with backslashes in them or smthn like that from breaking graphviz
+    if type(tree) in (float, int): return #probably shouldn't index numbers
+
+    #edges
     dot.edge(str(tree[id][0]) if type(tree[id][0]) in (float, int) else str(list(tree[id][0].keys())[0]), str(id))
     dot.edge(str(tree[id][1]) if type(tree[id][1]) in (float, int) else str(list(tree[id][1].keys())[0]), str(id))
 
+    #we need to go deeper
     visualizeTree(tree[id][0])
     visualizeTree(tree[id][1])
     
     
     
 
-
+55
 element_id = int(input("element id? "))
 element_tree = getElementTree(element_id)
 print(element_tree)
